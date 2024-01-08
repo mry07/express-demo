5230f7a9331a1d66e8970167d34f7c070d112dfd
Cahyo Sasongko <yyos.dev@gmail.com>
2024-01-08 21:15:16 +0700

    updated a auth web model

--- src/model/web/auth.ts
+++ src/model/web/auth.ts
@@ -6,3 +6,11 @@ export type LoginResponse = {
   access_token: string;
   refresh_token: string;
 };
+
+export type RefreshTokenRequest = {
+  refresh_token: string;
+};
+
+export type RefreshTokenResponse = {
+  access_token: string;
+};

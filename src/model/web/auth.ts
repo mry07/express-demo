e059c9fdc65a3dca00e5203e81e7d4a369129307
Cahyo Sasongko <yyos.dev@gmail.com>
2024-01-08 21:07:18 +0700

    updated a auth web model

--- src/model/web/auth.ts
+++ src/model/web/auth.ts
@@ -1,3 +1,8 @@
 import { User } from "../domain/user";
 
 export type LoginRequest = Pick<User, "email" | "password"> & {};
+
+export type LoginResponse = {
+  access_token: string;
+  refresh_token: string;
+};
